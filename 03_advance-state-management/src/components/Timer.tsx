a90406cf71fb10eac439276dbf833da47ec09141
Rashmi Raj <rashmiraj7877@gmail.com>
2024-09-03 02:30:45 +0530

    Updates Chapter 03: Advance State Management

--- 03_advance-state-management/src/components/Timer.tsx
+++ 03_advance-state-management/src/components/Timer.tsx
@@ -1,11 +1,44 @@
+import { useRef, useState, useEffect } from 'react';
+
 import Container from './ui/Container.tsx';
-import { type Timer as TimerProps } from '../store/TimersContext.tsx';
+import {
+  useTimersContext,
+  type Timer as TimerProps,
+} from '../store/TimersContext.tsx';
 
 const Timer = ({ name, duration }: TimerProps) => {
+  const interval = useRef<number | null>(null);
+  const [remainingTime, setRemainingTime] = useState(duration * 1000);
+  const { isRunning } = useTimersContext();
+
+  if (remainingTime <= 0 && interval.current) {
+    clearInterval(interval.current);
+  }
+
+  useEffect(() => {
+    let timer: number;
+
+    if (isRunning) {
+      timer = setInterval(() => {
+        setRemainingTime((prevTime) => prevTime - 1000);
+      }, 1000);
+      interval.current = timer;
+    } else if (interval.current) {
+      clearInterval(interval.current);
+    }
+
+    return () => clearInterval(timer);
+  });
+
+  const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
+
   return (
     <Container as='article'>
       <h2>{name}</h2>
-      <p>{duration}</p>
+      <p>
+        <progress max={duration * 1000} value={remainingTime} />
+      </p>
+      <p>{formattedRemainingTime}</p>
     </Container>
   );
 };
