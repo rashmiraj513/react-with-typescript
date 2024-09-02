ce85258d261e92a56fe642b7e358a3a63f9eaffc
Rashmi Raj <rashmiraj7877@gmail.com>
2024-09-03 02:31:32 +0530

    Adds Chapter 05: Redux with TypeScript

---
+++ 05_redux-with-typescript/src/components/Header.tsx
@@ -0,0 +1,36 @@
+import { useState } from 'react';
+
+import Cart from './Cart.tsx';
+import { useCartSelector } from '../store/hooks.ts';
+
+const Header = () => {
+  const [cartIsVisible, setCartIsVisible] = useState(false);
+  const cartQuantity = useCartSelector((state) =>
+    state.cart.items.reduce((val, item) => val + item.quantity, 0),
+  );
+
+  const handleOpenCartClick = () => {
+    setCartIsVisible(true);
+  };
+
+  const handleCloseCartClick = () => {
+    setCartIsVisible(false);
+  };
+
+  return (
+    <>
+      {cartIsVisible && <Cart onClose={handleCloseCartClick} />}
+      <header id='main-header'>
+        <div id='main-title'>
+          <img src='logo.png' alt='Elegant model' />
+          <h1>Elegant Redux</h1>
+        </div>
+        <p>
+          <button onClick={handleOpenCartClick}>Cart ({cartQuantity})</button>
+        </p>
+      </header>
+    </>
+  );
+};
+
+export default Header;
